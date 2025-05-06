659ef1bcd819a0c65312e5d10054314f03a845fe
Dima Jayyousi <dimajayousi@gmail.com>
2025-05-06 20:12:00 +0300

    the update of the upload from the dash board

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const multer = require('multer');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -11,6 +12,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // Auto-create uploads/images folder
